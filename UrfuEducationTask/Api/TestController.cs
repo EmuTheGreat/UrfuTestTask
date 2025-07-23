b6d89ce005700ec82d9b68cefc5dab9e68a9a233
EmuTheGreat <thegreatfrut@mail.ru>
2025-07-23 16:24:51 +0500

    -

---
+++ UrfuEducationTask/Api/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TestController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> TestMethod()
+    {
+        return Ok();
+    }
+}
